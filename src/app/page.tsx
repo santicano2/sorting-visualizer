46cb8155cf68d0fdbc56af958293bcae1681115e
Santi <canosantiago404@gmail.com>
2024-07-03 18:33:45 -0300

    añadido slider de velocidad de animacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useSortingAlhorithm } from "@/context/Visualizer";
-import { useEffect } from "react";
+import { Slider } from "@/components/input/Slider";
 
 export default function Home() {
-  const { arrayToSort, isSorting } = useSortingAlhorithm();
+  const { arrayToSort, isSorting, animationSpeed, setAnimationSpeed } =
+    useSortingAlhorithm();
 
   return (
     <main className="absolute top-0 h-screen w-screen z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
@@ -15,9 +16,15 @@ export default function Home() {
         >
           <div className="h-[66px] relative flex items-center justify-between w-full">
             <h1 className="text-gray-300 text-2xl font-light hidden md:flex">
-              Sorting Visualizer
+              Visualizador de ordenamiento
             </h1>
-            <div>Controls</div>
+            <div className="flex items-center justify-center gap-4">
+              <Slider
+                isDisabled={isSorting}
+                value={animationSpeed}
+                handleChange={(e) => setAnimationSpeed(Number(e.target.value))}
+              />
+            </div>
           </div>
           <div className="relative h-[calc(100vh-66px)] w-full">
             <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
